3c00fd2b816ff2fcf463b6ee3a5ca833a4ead647
J-David-Cortes <jhoann_cortes@soy.sena.edu.co>
2026-07-08 22:11:00 -0500

    Nueva clase usuario sin terminar

---
+++ Backend/modelos/usuario.php
@@ -0,0 +1,37 @@
+<?php
+
+    class Usuario{
+        //atributos
+        private $conexion;
+
+        public function __construct($conexion){
+            $this->conexion = $conexion;
+        }
+
+        //Metodos
+
+        public function consulta(){
+            $sql = "SELECT * FORM usuario ORDER BY nombre";
+            $res = mysqli_query($this->conexion, $sql) or die("No encontro la tabla USUARIO");
+
+            $vec = [];
+
+            while($row = mysql_fetch_array($res)){
+                $vec[] = $row;                
+            }
+
+            return $vec;
+        }
+
+        public function eliminar($id){
+            $sql = "DELETE FROM usurio WHERE id_usuario = $id";
+            mysqli_query($this->conexion, $sql) or die("NO elimino el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se elimino el registro";
+
+            return $vec;
+        }
+    }
+?>
