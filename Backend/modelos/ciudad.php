9e50b456656a6c1a2e741d962f30dc3697d09adc
J-David-Cortes <jhoann_cortes@soy.sena.edu.co>
2026-07-09 23:50:22 -0500

    Nuevo Modelo Ciudad sin terminar

---
+++ Backend/modelos/ciudad.php
@@ -0,0 +1,58 @@
+<?php
+    class ciudad{
+        //atributos
+        private $conexion;
+
+        public function __construct($conexion){
+            $this->conexion = $conexion;
+        }
+
+        //Metodos
+
+        public function consulta(){
+            $sql = "SELECT * FORM ciudad ORDER BY nombre";
+            $res = mysqli_query($this->conexion, $sql) or die("No encontro la tabla CIUDAD");
+
+            $vec = [];
+
+            while($row = mysql_fetch_array($res)){
+                $vec[] = $row;                
+            }
+
+            return $vec;
+        }
+
+        public function eliminar($id){
+            $sql = "DELETE FROM ciudad WHERE id_ciudad = $id";
+            mysqli_query($this->conexion, $sql) or die("NO elimino el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se elimino el registro";
+
+            return $vec;
+        }
+
+        public function insertar($params){
+            $sql = "INSERT INTO usuario(nombre, email, clave) VALUES('$params->nombre', '$params->email', '$params->clave')";
+            mysqli_query($this->conexion, $sql) or die("NO inserto el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se inserto el registro";
+
+            return $vec;
+        }
+
+        public function editar($id, $params){
+            $sql = "UPDATE usuario SET nombre = '$params->nombre' WHERE id_usuario = $id";
+            mysqli_query($this->conexion, $sql) or die("NO edito el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se edito el registro";
+
+            return $vec;
+        }
+    }
+?>
