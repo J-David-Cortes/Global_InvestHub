b3f78fabbc121c0ff0fdda3e7f0ec61c3c338eff
J-David-Cortes <jhoann_cortes@soy.sena.edu.co>
2026-07-12 17:04:43 -0500

    Creacion completa de modelo administrador

---
+++ Backend/modelos/administrador.php
@@ -0,0 +1,59 @@
+<?php
+
+    class administrador{
+        //atributos
+        private $conexion;
+
+        public function __construct($conexion){
+            $this->conexion = $conexion;
+        }
+
+        //Metodos
+
+        public function consulta(){
+            $sql = "SELECT * FORM administrador ORDER BY administrador";
+            $res = mysqli_query($this->conexion, $sql) or die("No encontro la tabla administrador");
+
+            $vec = [];
+
+            while($row = mysql_fetch_array($res)){
+                $vec[] = $row;                
+            }
+
+            return $vec;
+        }
+
+        public function eliminar($id){
+            $sql = "DELETE FROM administrador WHERE id_administrador = $id";
+            mysqli_query($this->conexion, $sql) or die("NO elimino el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se elimino el registro";
+
+            return $vec;
+        }
+
+        public function insertar($params){
+            $sql = "INSERT INTO administrador(fo_permiso_nivel, fo_area_cargo) VALUES $params->permiso_nivel, '$params->area_cargo')";
+            mysqli_query($this->conexion, $sql) or die("NO inserto el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se inserto el registro";
+
+            return $vec;
+        }
+
+        public function editar($id, $params){
+            $sql = "UPDATE administrador SET fo_permiso_nivel = $params->permiso_nivel, fo_area_cargo = '$params->area_cargo' WHERE id_usuario = $id";
+            mysqli_query($this->conexion, $sql) or die("NO edito el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se edito el registro";
+
+            return $vec;
+        }
+    }
+?>
