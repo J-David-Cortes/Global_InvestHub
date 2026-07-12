269bc10616d03910e6268a913b9136162e93aaf3
J-David-Cortes <jhoann_cortes@soy.sena.edu.co>
2026-07-12 17:27:59 -0500

    Creacion de modelo area_cargo

---
+++ Backend/modelos/area_cargo.php
@@ -0,0 +1,59 @@
+<?php
+
+    class area_cargo{
+        //atributos
+        private $conexion;
+
+        public function __construct($conexion){
+            $this->conexion = $conexion;
+        }
+
+        //Metodos
+
+        public function consulta(){
+            $sql = "SELECT * FORM area_cargo ORDER BY area_cargo";
+            $res = mysqli_query($this->conexion, $sql) or die("No encontro la tabla area_cargo");
+
+            $vec = [];
+
+            while($row = mysql_fetch_array($res)){
+                $vec[] = $row;                
+            }
+
+            return $vec;
+        }
+
+        public function eliminar($id){
+            $sql = "DELETE FROM area_cargo WHERE id_area_cargo = $id";
+            mysqli_query($this->conexion, $sql) or die("NO elimino el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se elimino el registro";
+
+            return $vec;
+        }
+
+        public function insertar($params){
+            $sql = "INSERT INTO area_cargo(nombre) VALUES('$params->nombre')";
+            mysqli_query($this->conexion, $sql) or die("NO inserto el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se inserto el registro";
+
+            return $vec;
+        }
+
+        public function editar($id, $params){
+            $sql = "UPDATE area_cargo SET nombre = '$params->nombre' WHERE id_usuario = $id";
+            mysqli_query($this->conexion, $sql) or die("NO edito el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se edito el registro";
+
+            return $vec;
+        }
+    }
+?>
