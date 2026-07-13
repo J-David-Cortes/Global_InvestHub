d776c8c5b48d030693fb6c791f48054ebd7404ec
J-David-Cortes <jhoann_cortes@soy.sena.edu.co>
2026-07-13 12:10:24 -0500

    Creacion de modulo titulo

---
+++ Backend/modelos/titulo.php
@@ -0,0 +1,59 @@
+<?php
+
+    class titulo{
+        //atributos
+        private $conexion;
+
+        public function __construct($conexion){
+            $this->conexion = $conexion;
+        }
+
+        //Metodos
+
+        public function consulta(){
+            $sql = "SELECT * FORM titulo ORDER BY titulo";
+            $res = mysqli_query($this->conexion, $sql) or die("No encontro la tabla TITULO");
+
+            $vec = [];
+
+            while($row = mysql_fetch_array($res)){
+                $vec[] = $row;                
+            }
+
+            return $vec;
+        }
+
+        public function eliminar($id){
+            $sql = "DELETE FROM titulo WHERE id_titulo = $id";
+            mysqli_query($this->conexion, $sql) or die("NO elimino el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se elimino el registro";
+
+            return $vec;
+        }
+
+        public function insertar($params){
+            $sql = "INSERT INTO titulo(fo_nivel_educacion, fo_leccion_educacion, fo_avance_educacion) VALUES('$params->nivel_educacion', '$params->leccion_educacion', '$params->avance_educacion')";
+            mysqli_query($this->conexion, $sql) or die("NO inserto el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se inserto el registro";
+
+            return $vec;
+        }
+
+        public function editar($id, $params){
+            $sql = "UPDATE titulo SET fo_nivel_educacion = '$params->nivel_educacion', fo_leccion_educacion = '$params->leccion_educacion', fo_avance_educacion = '$params->avance_educacion' WHERE id_titulo = $id";
+            mysqli_query($this->conexion, $sql) or die("NO edito el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se edito el registro";
+
+            return $vec;
+        }
+    }
+?>
