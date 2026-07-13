63e1517158a04c75eddc204312492815d9f8eb7d
J-David-Cortes <jhoann_cortes@soy.sena.edu.co>
2026-07-12 23:44:36 -0500

    Creacion del modulo avance_educacion

---
+++ Backend/modelos/avance_educacion.php
@@ -0,0 +1,59 @@
+<?php
+
+    class avance_educacion{
+        //atributos
+        private $conexion;
+
+        public function __construct($conexion){
+            $this->conexion = $conexion;
+        }
+
+        //Metodos
+
+        public function consulta(){
+            $sql = "SELECT * FORM avance_educacion ORDER BY avance_educacion";
+            $res = mysqli_query($this->conexion, $sql) or die("No encontro la tabla area_cargo");
+
+            $vec = [];
+
+            while($row = mysql_fetch_array($res)){
+                $vec[] = $row;                
+            }
+
+            return $vec;
+        }
+
+        public function eliminar($id){
+            $sql = "DELETE FROM avance_educacion WHERE id_avance_educacion = $id";
+            mysqli_query($this->conexion, $sql) or die("NO elimino el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se elimino el registro";
+
+            return $vec;
+        }
+
+        public function insertar($params){
+            $sql = "INSERT INTO avance_educacion(nombre) VALUES('$params->nombre')";
+            mysqli_query($this->conexion, $sql) or die("NO inserto el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se inserto el registro";
+
+            return $vec;
+        }
+
+        public function editar($id, $params){
+            $sql = "UPDATE avance_educacion SET nombre = '$params->nombre' WHERE id_avance_educacion = $id";
+            mysqli_query($this->conexion, $sql) or die("NO edito el REGISTRO");
+
+            $vec = [];
+            $vec['Resultado'] = "OK";
+            $vec['mensaje'] = "Se edito el registro";
+
+            return $vec;
+        }
+    }
+?>
