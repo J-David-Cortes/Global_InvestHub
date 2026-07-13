5aa7a94b5afed19ae01b1ace16b3e44ac41fa389
J-David-Cortes <jhoann_cortes@soy.sena.edu.co>
2026-07-13 00:07:56 -0500

    Creacion de modelo bot

--- Backend/modelos/bot.php
+++ Backend/modelos/bot.php
@@ -1,5 +1,6 @@
 <?php
-    class ciudad{
+
+    class bot{
         //atributos
         private $conexion;
 
@@ -10,21 +11,8 @@
         //Metodos
 
         public function consulta(){
-            $sql = "SELECT * FORM ciudad ORDER BY nombre";
-            $res = mysqli_query($this->conexion, $sql) or die("No encontro la tabla CIUDAD");
-
-            $vec = [];
-
-            while($row = mysql_fetch_array($res)){
-                $vec[] = $row;                
-            }
-
-            return $vec;
-        }
-
-        public function consulta2($id_departamento){
-            $sql = "SELECT * FORM ciudad WHERE fo_departamento = $id_departamento ORDER BY nombre";
-            $res = mysqli_query($this->conexion, $sql) or die("No encontro la tabla CIUDAD");
+            $sql = "SELECT * FORM bot ORDER BY nombre";
+            $res = mysqli_query($this->conexion, $sql) or die("No encontro la tabla area_cargo");
 
             $vec = [];
 
@@ -36,7 +24,7 @@
         }
 
         public function eliminar($id){
-            $sql = "DELETE FROM ciudad WHERE id_ciudad = $id";
+            $sql = "DELETE FROM bot WHERE id_bot = $id";
             mysqli_query($this->conexion, $sql) or die("NO elimino el REGISTRO");
 
             $vec = [];
@@ -47,7 +35,7 @@
         }
 
         public function insertar($params){
-            $sql = "INSERT INTO bot(nombre, fo_departamento) VALUES('$params->nombre', '$params->departamento')";
+            $sql = "INSERT INTO bot(nombre, codigo, fo_usuario, fo_api) VALUES('$params->nombre', '$params->codigo', '$params->usuario', '$params->api')";
             mysqli_query($this->conexion, $sql) or die("NO inserto el REGISTRO");
 
             $vec = [];
@@ -58,7 +46,7 @@
         }
 
         public function editar($id, $params){
-            $sql = "UPDATE ciudad SET nombre = '$params->nombre', fo_departamento = '$params->departamento' WHERE id_ciudad = $id";
+            $sql = "UPDATE bot SET nombre = '$params->nombre', codigo = '$params->codigo', fo_usuario = '$params->usuario', fo_api = '$params->api' WHERE id_usuario = $id";
             mysqli_query($this->conexion, $sql) or die("NO edito el REGISTRO");
 
             $vec = [];
